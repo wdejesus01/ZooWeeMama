b5639634bbd605b96bca4e0dd5958b9433e226e5
willieWonkajr <wfjdbidub@gmail.com>
2023-04-10 15:23:17 -0400

    Version 2

--- ZooWeeMama.sql
+++ ZooWeeMama.sql
@@ -1,24 +1,29 @@
-CREATE TABLE ORDER(
-    ID CHAR(10) PRIMARY KEY,
-    ord_date DATE);
+CREATE TABLE ORDERS(
+    ID CHAR(10),
+    ord_date date,
+    PRIMARY KEY(ID));
 
 CREATE TABLE VISITOR(
-    ID CHAR(10) PRIMARY KEY
-        name varchar(255)
+    ID CHAR(10) PRIMARY KEY,
+        name varchar(255),
         age TINYINT);
 
 CREATE TABLE SPECIES(
     family varchar(255) PRIMARY KEY,
     origin varchar(255),
-    desc TEXT);
+    description TEXT);
 
-CREATE TABLE ANIMAL(
-    name varchar(255) PRIMARY KEY,
-    age CHAR(4),
-    count TINYINT,
+
+CREATE TABLE ANIMAL
+(
+    name   varchar(255) PRIMARY KEY,
+    age    CHAR(4),
+    count  TINYINT,
     f_name varchar(255),
-    FOREIGN KEY (f_name) REFERENCES SPECIES(family));
-
+    e_name varchar(255),
+    FOREIGN KEY (f_name) REFERENCES SPECIES (family),
+    FOREIGN KEY (e_name) REFERENCES EXHIBIT (name)
+);
 CREATE TABLE BUILDING(
     name varchar(255) PRIMARY KEY,
     year char(4));
@@ -30,7 +35,7 @@ CREATE TABLE DEPARTMENT(
     FOREIGN KEY (b_name) REFERENCES BUILDING(name));
 
 CREATE TABLE EMPLOYEE(
-    ID char(10),
+    ID char(10) PRIMARY KEY,
     name varchar(255),
     position varchar(255),
     start_date DATE,
@@ -40,25 +45,34 @@ CREATE TABLE EMPLOYEE(
     d_no char(10),
     FOREIGN KEY (d_no) REFERENCES DEPARTMENT(ID));
 
-ALTER TABLE DEPARTMENT(
-ADD e_id char(10),
-ADD FOREIGN KEY (e_id) REFERENCES EMPLOYEE(ID));
 
-CREATE TABLE EVENTS(
+ALTER TABLE DEPARTMENT
+ADD e_id char(10),
+ADD CONSTRAINT FOREIGN KEY(e_id) references EMPLOYEE (ID);
+
+CREATE TABLE EVENT(
     name varchar(255),
-    time DATE,
+    time Date,
     cost float,
     capacity int,
-    b_name,
+    b_name varchar(255),
+    primary key (name, time),
     FOREIGN KEY(b_name)REFERENCES BUILDING(name));
+
+CREATE TABLE EXHIBIT(
+	name varchar(255) PRIMARY KEY,
+	biome varchar(255),
+	b_name varchar(255),
+	foreign key(b_name) REFERENCES BUILDING(name)
+);
 
 CREATE TABLE STORE(
     name varchar(255) PRIMARY KEY,
-    type varchar(255)
-    b_name varchar(255)
+    type varchar(255),
+    b_name varchar(255),
     FOREIGN KEY (b_name) REFERENCES BUILDING(name));
 
-CREATE TABLE ITEMS(
+CREATE TABLE ITEM(
     ID char(10), amount INT, cost DECIMAL(3,2), s_name varchar(255),
     FOREIGN KEY(s_name) REFERENCES STORE(name));
 
@@ -69,19 +83,24 @@ CREATE TABLE WORKS_EXHIBIT(
 	FOREIGN KEY (ex_name) REFERENCES EXHIBIT(name), 
 	FOREIGN KEY (e_id) REFERENCES EMPLOYEE(ID));
 
-CREATE TABLE WORKS_EVENT(
-	ev_name varchar(255), 
-	ev_time date,
-	e_id char(10),
-	PRIMARY KEY(e_id, ev_time,ev_name)
-	FOREIGN KEY (ev_name) REFERENCES EVENT(name),
-	FOREIGN KEY (ev_time) REFERENCES EVENT(time), 
-	FOREIGN KEY (e_id) REFERENCES EMPLOYEE(ID));
+
+CREATE TABLE WORKS_EVENT
+(
+    ev_name varchar(255),
+    ev_time date,
+    e_id    char(10),
+    primary key (ev_name, ev_time, e_id),
+        foreign key (ev_name) REFERENCES EVENT(name),
+        foreign key (ev_time) REFERENCES EVENT(time),
+        foreign key(e_id) REFERENCES EMPLOYEE(ID)
+);
+
+DROP TABLE WORKS_EVENT;
 
 CREATE TABLE WORKS_STORE(
        	s_name varchar(255),
-	e_id char(10), 
-	PRIMARY KEY(e_id,s_name)
+	e_id char(10),
+	PRIMARY KEY(e_id,s_name),
 	FOREIGN KEY (s_name) REFERENCES STORE(name),
 	FOREIGN KEY (e_id) REFERENCES EMPLOYEE(ID));
 
@@ -90,13 +109,14 @@ CREATE TABLE PURCHASES(
 	i_id char(10),
 	ord_id char(10),
 	v_id char(10),
+	primary key (s_name,i_id,ord_id,v_id),
 	FOREIGN KEY(s_name) REFERENCES STORE(name),
-	FOREIGN KEY(i_id)REFERENCES ITEM(ID),
-	FOREIGN KEY(ord_id) REFERENCES ORDER(ID),
+	FOREIGN KEY(i_id) REFERENCES ITEM(ID),
+	FOREIGN KEY(ord_id) REFERENCES ORDERS(ID),
 	FOREIGN KEY(v_id) REFERENCES VISITOR(ID));
 
 CREATE TABLE FOUND_IN(
 	s_family varchar(255), ex_name varchar(255),
 	PRIMARY KEY(s_family,ex_name),
-	FOREIGN KEY (s_family) REFERENCES SPECIES(family);
+	FOREIGN KEY (s_family) REFERENCES SPECIES(family),
 	FOREIGN KEY (ex_name) REFERENCES EXHIBIT(name));
